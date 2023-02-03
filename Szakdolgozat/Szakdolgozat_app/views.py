dbed1eb1b23f4113cc22beb697da5538020e4f5e
Szlúka András Tamás <h123456@stud.u-szeged.hu>
2023-02-04 00:21:19 +0100

    Gombok létrehozása, állás visszatöltésének lehetősége

--- Szakdolgozat/Szakdolgozat_app/views.py
+++ Szakdolgozat/Szakdolgozat_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Position
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'Szakdolgozat_app/index.html')
+
+    context={}
+
+    all_positions= Position.objects.all().order_by('date_of_step').reverse()# a lépések kiíratása sorrendben, legfrissebb legfelül
+    context['all_positions'] = all_positions # dictionary
+    return render(request, 'Szakdolgozat_app/index.html', context) # és átadjuk
     #return HttpResponse('Hello!')

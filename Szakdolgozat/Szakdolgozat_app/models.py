e91c64152d91577eca9b02fa76a3ccd7ae394877
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2023-02-04 14:53:40 +0100

    Választási lehetőségek ajánlása

--- Szakdolgozat/Szakdolgozat_app/models.py
+++ Szakdolgozat/Szakdolgozat_app/models.py
@@ -6,8 +6,10 @@ rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR
 '''
 
 class Position(models.Model):
+    FEKETE_VAGY_FEHER_LEETOSEGEK = (("White", "White"),("Black", "Black"),("New", "New"))
+
     positions_of_figures = models.CharField(max_length=64, default='rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR')
     step_count = models.DecimalField(max_digits=10, decimal_places=0, default='')
     player_name = models.CharField(max_length=30, default='')
-    white_or_black = models.CharField(max_length=5, default='New')
+    white_or_black = models.CharField(max_length=5, default='New', choices=FEKETE_VAGY_FEHER_LEETOSEGEK)
     date_of_step = models.DateTimeField(auto_now_add=True)

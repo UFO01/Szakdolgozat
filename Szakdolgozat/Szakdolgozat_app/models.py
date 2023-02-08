ae5599d327b73430e3ef62048f8609cba8ab0014
Szlúka András Tamás <h123456@stud.u-szeged.hu>
2023-02-08 20:46:13 +0100

    A form feldolgozása (egyelőre hibás)

--- Szakdolgozat/Szakdolgozat_app/models.py
+++ Szakdolgozat/Szakdolgozat_app/models.py
@@ -6,10 +6,10 @@ rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR
 '''
 
 class Position(models.Model):
-    FEKETE_VAGY_FEHER_LEETOSEGEK = (("White", "White"),("Black", "Black"),("New", "New"))
+    Black_or_White_Choices = (("White", "White"),("Black", "Black"),("New", "New"))
 
     positions_of_figures = models.CharField(max_length=64, default='rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR')
-    step_count = models.DecimalField(max_digits=10, decimal_places=0, default='')
+    step_count = models.DecimalField(max_digits=10, decimal_places=0, default=0)
     player_name = models.CharField(max_length=30, default='')
-    white_or_black = models.CharField(max_length=5, default='New', choices=FEKETE_VAGY_FEHER_LEETOSEGEK)
+    white_or_black = models.CharField(max_length=5, default='New', choices=Black_or_White_Choices)
     date_of_step = models.DateTimeField(auto_now_add=True)
--- Szakdolgozat/Szakdolgozat_app/views.py
+++ Szakdolgozat/Szakdolgozat_app/views.py
@@ -8,7 +8,32 @@ def index(request):
 
     context={}
 
+    if request.method=='POST':
+        if request.POST.get('positions') != Position.objects.last().positions_of_figures:
+            p = Position()
+            p.positions_of_figures = request.POST.get('positions')
+            if p.positions_of_figures == 'rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR':
+                p.step_count=0
+                p.white_or_black= "New"
+            else:
+                p.step_count = Position.objects.last().step_count + 1
+                if Position.objects.last().white_or_black in ("Black", "New"):
+                    p.white_or_black = "White"
+                else:
+                    p.white_or_black = "Black"
+
+
+
+
+
+
+            p.player_name= request.POST.get('name')
+            p.save()
+        else:
+            context['error'] = 'Te vagy lépésen!'
+
+
+
     all_positions= Position.objects.all().order_by('date_of_step').reverse()# a lépések kiíratása sorrendben, legfrissebb legfelül
     context['all_positions'] = all_positions # dictionary
     return render(request, 'Szakdolgozat_app/index.html', context) # és átadjuk
-    #return HttpResponse('Hello!')

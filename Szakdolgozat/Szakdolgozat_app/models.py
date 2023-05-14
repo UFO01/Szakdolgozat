5a84098fc6c9878d034aa31130470a450335c22b
Szlúka András Tamás <h123456@stud.u-szeged.hu>
2023-05-14 11:07:01 +0200

    Sakkadás felismerése

--- Szakdolgozat/Szakdolgozat_app/models.py
+++ Szakdolgozat/Szakdolgozat_app/models.py
@@ -7,7 +7,6 @@ rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR
 
 class Position(models.Model):
     Black_or_White_Choices = (("White", "White"),("Black", "Black"),("New", "New"))
-
     positions_of_figures = models.CharField(max_length=64, default='rnbqkbnrppppppppxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPPPPPPPPRNBQKBNR')
     step_count = models.DecimalField(max_digits=10, decimal_places=0, default=0)
     player_name = models.CharField(max_length=30, default='')
